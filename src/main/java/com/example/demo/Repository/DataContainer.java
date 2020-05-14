531c8d7c5ec5990a214fab11a441e02cc429924e
Immanuellllllll <44088357+Immanuellllllll@users.noreply.github.com>
2020-05-14 10:44:43 +0200

    Sync funktion godt på vej

---
+++ src/main/java/com/example/demo/Repository/DataContainer.java
@@ -0,0 +1,9 @@
+package com.example.demo.Repository;
+
+import java.net.Socket;
+import java.util.*;
+
+public class DataContainer {
+   public static Map<Integer, ArrayList<Socket>> Clientlistmap=new Hashtable<>();
+
+}
--- src/main/java/com/example/demo/Service/AppService.java
+++ src/main/java/com/example/demo/Service/AppService.java
@@ -1,6 +1,7 @@
 package com.example.demo.Service;
 
 import com.example.demo.Model.Music;
+import com.example.demo.Repository.DataContainer;
 import com.example.demo.Repository.MusicRepo;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,35 +15,42 @@ import java.net.Socket;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Hashtable;
 import java.util.List;
 import java.net.Socket;
+import java.util.Map;
 
 @Service
 public class AppService {
+    Map<String,Socket> usersockets=new Hashtable<>();
     ServerSocket ss;
     Socket s;
-    DataOutputStream out;
-    DataInputStream in;
+    DataOutputStream sout;
+    DataInputStream sin;
+    Clienthandler ch;
     @Autowired
     private MusicRepo mr;
 
     public AppService() throws IOException {
     }
 
-    public void startClient() throws IOException {
+    public void startClient(String groupid,String userid) throws IOException {
         s=new Socket("localhost",8000);
-        in=new DataInputStream(s.getInputStream());
+        DataOutputStream cos=new DataOutputStream(s.getOutputStream());
+        cos.writeUTF(groupid);
+        usersockets.put(userid,s);
+
 
     }
 
-    public Object waitForHost() throws IOException {
-        String msg = in.readUTF();
-        System.out.println(msg);
+    public Object waitForHost(String userid) throws IOException {
+        DataInputStream csin=new DataInputStream(usersockets.get(userid).getInputStream());
+        String msg = csin.readUTF();
+        System.out.println("got: "+msg);
         Music music = new Music();
         String msga[]=msg.split(";");
         music.setPath(msga[0]);
         music.setTime_stamp(msga[1]);
-
         return music;
     }
 
@@ -50,20 +58,46 @@ public class AppService {
         return mr.findAll();
     }
 
-    public void sendTrack(String path,String time) throws IOException {
-        Socket client=ss.accept();
-        out=new DataOutputStream(client.getOutputStream());
-        Float hours = Float.valueOf(time) / 3600;
-        Float minutes = (Float.valueOf(time) % 3600) / 60;
-        Float seconds = Float.valueOf(time) % 60;
-        String timeString = String.format("%02d:%02d:%02d", Math.round(hours), Math.round(minutes), Math.round(seconds));
-        System.out.println(path+";"+time);
-        out.writeUTF(path+";"+timeString);
-
+    public void sendTrack(String path,String time,int groupid) throws IOException {
+        for (int i =0;i<DataContainer.Clientlistmap.get(groupid).size();i++) {
+            List<Socket> grouplist=DataContainer.Clientlistmap.get(groupid);
+            sout = new DataOutputStream(grouplist.get(i).getOutputStream());
+            Float hours = Float.valueOf(time) / 3600;
+            Float minutes = (Float.valueOf(time) % 3600) / 60;
+            Float seconds = Float.valueOf(time) % 60;
+            String timeString = String.format("%02d:%02d:%02d", Math.round(hours), Math.round(minutes), Math.round(seconds));
+            System.out.println("Send: "+path + ";" + time);
+            sout.writeUTF(path + ";" + timeString);
+        }
     }
 
     public void openServerSocket() throws IOException {
-       ss=new ServerSocket(8000);
+        DataContainer.Clientlistmap.put(1,new ArrayList<Socket>());
+        DataContainer.Clientlistmap.put(2,new ArrayList<Socket>());
+        ss=new ServerSocket(8000);
+       ch=new Clienthandler();
+       Thread t = new Thread(ch);
+       t.start();
     }
 
+
+    public class Clienthandler implements Runnable {
+        @Override
+        public void run() {
+            while (true){
+                try {
+                    Socket client=ss.accept();
+                    DataInputStream sin=new DataInputStream(client.getInputStream());
+                    Integer groupid = Integer.valueOf(sin.readUTF());
+                    DataContainer.Clientlistmap.get(groupid).add(client);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+
+
+        }
+    }
 }
