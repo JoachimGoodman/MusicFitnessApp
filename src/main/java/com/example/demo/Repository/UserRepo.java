ae61f9d6f4a8755d6d233802890a3fc1b07cb69c
Immanuellllllll <44088357+Immanuellllllll@users.noreply.github.com>
2020-05-21 11:18:51 +0200

    Sync funktion godt på vej

--- src/main/java/com/example/demo/Repository/UserRepo.java
+++ src/main/java/com/example/demo/Repository/UserRepo.java
@@ -17,7 +17,13 @@ public interface UserRepo extends JpaRepository<User, Integer> {
     List<User> findAll();
 
     @Modifying
-    @Query(value = "insert into users (username,password,enabled) VALUES (:username,:password,:enabled)", nativeQuery = true)
+    @Query(value = "insert into users (username,password,enabled) VALUES (:username,:password,:enabled);", nativeQuery = true)
     @Transactional
     void logUser(@Param("username") String username, @Param("password") String password,@Param("enabled") boolean enabled);
+
+    @Modifying
+    @Query(value = "insert into authorities values (:username,:role);", nativeQuery = true)
+    @Transactional
+    void logPermission(@Param("username") String username,@Param("role")String role);
 }
+
