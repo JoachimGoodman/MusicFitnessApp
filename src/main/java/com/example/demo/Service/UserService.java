56f73507b6cd42962e14a85da9a4809e4bcca62d
Immanuellllllll <44088357+Immanuellllllll@users.noreply.github.com>
2020-05-21 13:52:15 +0200

    Hashing a password i userservice

--- src/main/java/com/example/demo/Service/UserService.java
+++ src/main/java/com/example/demo/Service/UserService.java
@@ -4,6 +4,7 @@ package com.example.demo.Service;
 import com.example.demo.Model.User;
 import com.example.demo.Repository.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -14,6 +15,9 @@ public class UserService {
     @Autowired
     private UserRepo ur;
 
+
+    @Autowired
+    BCryptPasswordEncoder bCryptPasswordEncoder;
     public UserService() throws IOException {
     }
 
@@ -23,7 +27,7 @@ public class UserService {
     }
 
     public void createUser(User user) {
-        ur.logUser(user.getUsername(), user.getPassword(), true, user.getFirstname(), user.getLastname(), user.getEmail());
+        ur.logUser(user.getUsername(), bCryptPasswordEncoder.encode(user.getPassword()), true, user.getFirstname(), user.getLastname(), user.getEmail());
         ur.logPermission(user.getUsername(), "ROLE_BRUGER");
     }
 
